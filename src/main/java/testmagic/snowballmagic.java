9632d8e3f21315829c92c1c6e67a97a52af6fc1e
SvenBrnn <svenbrnn@web.de>
2012-03-05 17:10:30 +0100

    Add Damage to SnowBallMagic

--- src/main/java/testmagic/snowballmagic.java
+++ src/main/java/testmagic/snowballmagic.java
@@ -2,6 +2,13 @@ package testmagic;
 
 
 import bluenova.fairytailcraft.plugin.MagePluginEvent;
+import net.minecraft.server.Block;
+import net.minecraft.server.Item;
+import org.bukkit.Material;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.Projectile;
+import org.bukkit.event.entity.ProjectileHitEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 
 /*
@@ -14,10 +21,21 @@ import org.bukkit.event.player.PlayerInteractEvent;
  * @author Sven
  */
 public class snowballmagic extends MagePluginEvent {
+    Material lastActiveItem;
 
     @Override
     public void callPlayerInteractEvent(PlayerInteractEvent event) {
         event.getPlayer().throwSnowball();
         event.getPlayer().sendMessage("Mana decreesed!");
-    }    
+        lastActiveItem = event.getItem().getType();
+    }
+
+    @Override
+    public void callPlayerHitByProjectilEvent(ProjectileHitEvent event) {
+        if(lastActiveItem == Material.AIR) {
+            Projectile proj = (Projectile) event.getEntity();
+            Player target = (Player)proj.getNearbyEntities(1, 1, 1).get(0);
+            target.damage(2, proj.getShooter());            
+        }
+    }
 }
