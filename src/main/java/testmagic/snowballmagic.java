303ac5219b4e34ed4da87a5d5c5c5b3ec2d0cac0
SvenBrnn <svenbrnn@web.de>
2012-03-07 15:35:16 +0100

    Some Changes for Event Change

--- src/main/java/testmagic/snowballmagic.java
+++ src/main/java/testmagic/snowballmagic.java
@@ -1,11 +1,10 @@
 package testmagic;
 
-
 import bluenova.fairytailcraft.plugin.MagePluginEvent;
-import net.minecraft.server.Block;
-import net.minecraft.server.Item;
+import java.lang.annotation.Target;
 import org.bukkit.Material;
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Projectile;
 import org.bukkit.event.entity.ProjectileHitEvent;
@@ -15,12 +14,12 @@ import org.bukkit.event.player.PlayerInteractEvent;
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author Sven
  */
 public class snowballmagic extends MagePluginEvent {
+
     Material lastActiveItem;
 
     @Override
@@ -31,11 +30,14 @@ public class snowballmagic extends MagePluginEvent {
     }
 
     @Override
-    public void callPlayerHitByProjectilEvent(ProjectileHitEvent event) {
-        if(lastActiveItem == Material.AIR) {
+    public void callEntityHitByProjectilEvent(ProjectileHitEvent event) {
+        if (lastActiveItem == Material.AIR) {
             Projectile proj = (Projectile) event.getEntity();
-            Player target = (Player)proj.getNearbyEntities(1, 1, 1).get(0);
-            target.damage(2, proj.getShooter());            
+            Entity tar = proj.getNearbyEntities(1, 1, 1).get(0);
+            if (tar instanceof LivingEntity) {
+                LivingEntity target = (LivingEntity) tar;
+                target.damage(2, proj.getShooter());
+            }
         }
     }
 }
