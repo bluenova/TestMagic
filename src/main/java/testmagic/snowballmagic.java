8bd8c2c59d0210366052f54ca028005faae2989b
SvenBrnn <svenbrnn@web.de>
2012-03-08 13:52:57 +0100

    Some bugfixes

--- src/main/java/testmagic/snowballmagic.java
+++ src/main/java/testmagic/snowballmagic.java
@@ -1,13 +1,14 @@
 package testmagic;
 
 import bluenova.fairytailcraft.plugin.MagePluginEvent;
-import org.bukkit.Material;
+import java.util.HashMap;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
 import org.bukkit.entity.Projectile;
-import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.ProjectileHitEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
 
 /*
  * To change this template, choose Tools | Templates
@@ -19,24 +20,26 @@ import org.bukkit.event.player.PlayerInteractEvent;
  */
 public class snowballmagic extends MagePluginEvent {
 
-    Material lastActiveItem;
+    HashMap<Player, ItemStack> lastActiveItem = new HashMap<Player, ItemStack>();
 
     @Override
     public void callPlayerInteractEvent(PlayerInteractEvent event) {
         event.getPlayer().throwSnowball();
         event.getPlayer().sendMessage("Mana decreesed!");
-        lastActiveItem = event.getItem().getType();
+        lastActiveItem.put(event.getPlayer(), event.getItem());
     }
 
     @Override
     public void callEntityHitByProjectilEvent(ProjectileHitEvent event) {
-                   if (lastActiveItem == Material.AIR) {
-            Projectile proj = (Projectile) event.getEntity();
-            Entity tar = proj.getNearbyEntities(1, 1, 1).get(0);
-            if (tar instanceof LivingEntity) {
-                LivingEntity target = (LivingEntity) tar;
-                target.damage(2, proj.getShooter());
+        if (event.getEntity().getShooter() instanceof Player) {
+            if (lastActiveItem.get((Player)event.getEntity().getShooter()) == null) {
+                Projectile proj = (Projectile) event.getEntity();
+                Entity tar = proj.getNearbyEntities(1, 1, 1).get(0);
+                if (tar instanceof LivingEntity) {
+                    LivingEntity target = (LivingEntity) tar;
+                    target.damage(2, proj.getShooter());
+                }
             }
         }
-    }     
+    }
 }
