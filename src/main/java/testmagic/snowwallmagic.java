4f988f20e3d8ca0e0ad7cb6e30d180a4c0c79374
SvenBrnn <svenbrnn@web.de>
2012-03-15 20:11:30 +0100

    Fix some Bugs

--- src/main/java/testmagic/snowwallmagic.java
+++ src/main/java/testmagic/snowwallmagic.java
@@ -10,6 +10,7 @@ import java.util.List;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.util.Vector;
 
@@ -19,124 +20,127 @@ import org.bukkit.util.Vector;
  */
 public class snowwallmagic extends MagePluginEvent {
 
-    @Override
+      @Override
     public boolean callPlayerInteractEvent(PlayerInteractEvent event) {
-        Player player = event.getPlayer();
-        Vector dir = player.getTargetBlock(null, 200).getLocation().toVector();
-        Location pos = player.getLocation();
-        Location[] directArr = new Location[]{
-            new Location(pos.getWorld(), pos.getBlockX() + 1, pos.getBlockY(), pos.getBlockZ()),
-            new Location(pos.getWorld(), pos.getBlockX() - 1, pos.getBlockY(), pos.getBlockZ()),
-            new Location(pos.getWorld(), pos.getBlockX(), pos.getBlockY(), pos.getBlockZ() + 1),
-            new Location(pos.getWorld(), pos.getBlockX(), pos.getBlockY(), pos.getBlockZ() - 1)
-        };
-        Location[] edgesArr = new Location[]{
-            new Location(pos.getWorld(), pos.getBlockX() + 1, pos.getBlockY(), pos.getBlockZ() + 1),
-            new Location(pos.getWorld(), pos.getBlockX() - 1, pos.getBlockY(), pos.getBlockZ() - 1),
-            new Location(pos.getWorld(), pos.getBlockX() - 1, pos.getBlockY(), pos.getBlockZ() + 1),
-            new Location(pos.getWorld(), pos.getBlockX() + 1, pos.getBlockY(), pos.getBlockZ() - 1)
-        };
-        double minDis = 100000.0;
-        double distance = 0.0;
-        Location loc = null;
-        boolean isEdge = false;
-        wallDirections dirrect = wallDirections.NORTH;
-        for (int i = 0; i < 4; i++) {
-            distance = dir.distance(directArr[i].toVector());
-            if (distance < minDis) {
-                minDis = distance;
-                loc = directArr[i];
-                switch (i) {
-                    case 0:
-                        dirrect = wallDirections.NORTH;
-                        break;
-                    case 1:
-                        dirrect = wallDirections.SOUTH;
-                        break;
-                    case 2:
-                        dirrect = wallDirections.WEST;
-                        break;
-                    case 3:
-                        dirrect = wallDirections.EAST;
-                        break;
+        if (event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK) {
+            Player player = event.getPlayer();
+            Vector dir = player.getTargetBlock(null, 200).getLocation().toVector();
+            Location pos = player.getLocation();
+            Location[] directArr = new Location[]{
+                new Location(pos.getWorld(), pos.getBlockX() + 1, pos.getBlockY(), pos.getBlockZ()),
+                new Location(pos.getWorld(), pos.getBlockX() - 1, pos.getBlockY(), pos.getBlockZ()),
+                new Location(pos.getWorld(), pos.getBlockX(), pos.getBlockY(), pos.getBlockZ() + 1),
+                new Location(pos.getWorld(), pos.getBlockX(), pos.getBlockY(), pos.getBlockZ() - 1)
+            };
+            Location[] edgesArr = new Location[]{
+                new Location(pos.getWorld(), pos.getBlockX() + 1, pos.getBlockY(), pos.getBlockZ() + 1),
+                new Location(pos.getWorld(), pos.getBlockX() - 1, pos.getBlockY(), pos.getBlockZ() - 1),
+                new Location(pos.getWorld(), pos.getBlockX() - 1, pos.getBlockY(), pos.getBlockZ() + 1),
+                new Location(pos.getWorld(), pos.getBlockX() + 1, pos.getBlockY(), pos.getBlockZ() - 1)
+            };
+            double minDis = 100000.0;
+            double distance = 0.0;
+            Location loc = null;
+            boolean isEdge = false;
+            wallDirections dirrect = wallDirections.NORTH;
+            for (int i = 0; i < 4; i++) {
+                distance = dir.distance(directArr[i].toVector());
+                if (distance < minDis) {
+                    minDis = distance;
+                    loc = directArr[i];
+                    switch (i) {
+                        case 0:
+                            dirrect = wallDirections.NORTH;
+                            break;
+                        case 1:
+                            dirrect = wallDirections.SOUTH;
+                            break;
+                        case 2:
+                            dirrect = wallDirections.WEST;
+                            break;
+                        case 3:
+                            dirrect = wallDirections.EAST;
+                            break;
+                    }
+                }
+
+            }
+            for (int i = 0; i < 4; i++) {
+                distance = dir.distance(edgesArr[i].toVector());
+
+                if (distance < minDis) {
+                    minDis = distance;
+                    loc = edgesArr[i];
+                    isEdge = true;
+                    switch (i) {
+
+                        case 0:
+                            dirrect = wallDirections.NORTHWEST;
+                            break;
+                        case 1:
+                            dirrect = wallDirections.SOUTHEAST;
+                            break;
+                        case 2:
+                            dirrect = wallDirections.SOUTHWEST;
+                            break;
+                        case 3:
+                            dirrect = wallDirections.NORTHEAST;
+                            break;
+                    }
                 }
             }
 
-        }
-        for (int i = 0; i < 4; i++) {
-            distance = dir.distance(edgesArr[i].toVector());
-                           
-            if (distance < minDis) {
-                minDis = distance;
-                loc = edgesArr[i];
-                isEdge = true;
-                switch (i) {
+            Location[] wallArr;
+            //Generate Wall
+            if (!isEdge) {
+                if (dirrect == wallDirections.NORTH || dirrect == wallDirections.SOUTH) {
+                    MagePluginRegion regio = new MagePluginRegion(new Location(loc.getWorld(), loc.getBlockX(), loc.getBlockY(), loc.getBlockZ() - 2), new Location(loc.getWorld(), loc.getBlockX(), loc.getBlockY() + 2, loc.getBlockZ() + 2), loc.getWorld());
+                    wallArr = regio.getLocations().toArray(new Location[0]);
+                } else {
+                    MagePluginRegion regio = new MagePluginRegion(new Location(loc.getWorld(), loc.getBlockX() - 2, loc.getBlockY(), loc.getBlockZ()), new Location(loc.getWorld(), loc.getBlockX() + 2, loc.getBlockY() + 2, loc.getBlockZ()), loc.getWorld());
+                    wallArr = regio.getLocations().toArray(new Location[0]);
+                }
+            } else {
+                if (dirrect == wallDirections.NORTHEAST) {
+                    MagePluginRegion regio = new MagePluginRegion(new Location(loc.getWorld(), loc.getBlockX(), loc.getBlockY(), loc.getBlockZ()), new Location(loc.getWorld(), loc.getBlockX() - 2, loc.getBlockY() + 2, loc.getBlockZ()), loc.getWorld());
+                    MagePluginRegion regio2 = new MagePluginRegion(new Location(loc.getWorld(), loc.getBlockX(), loc.getBlockY(), loc.getBlockZ()), new Location(loc.getWorld(), loc.getBlockX(), loc.getBlockY() + 2, loc.getBlockZ() + 2), loc.getWorld());
+                    List<Location> locations = regio.getLocations();
+                    List<Location> locations2 = regio2.getLocations();
+                    locations.addAll(locations2);
+                    wallArr = locations.toArray(new Location[0]);
+                } else if (dirrect == wallDirections.NORTHWEST) {
+                    MagePluginRegion regio = new MagePluginRegion(new Location(loc.getWorld(), loc.getBlockX(), loc.getBlockY(), loc.getBlockZ()), new Location(loc.getWorld(), loc.getBlockX() - 2, loc.getBlockY() + 2, loc.getBlockZ()), loc.getWorld());
+                    MagePluginRegion regio2 = new MagePluginRegion(new Location(loc.getWorld(), loc.getBlockX(), loc.getBlockY(), loc.getBlockZ()), new Location(loc.getWorld(), loc.getBlockX(), loc.getBlockY() + 2, loc.getBlockZ() - 2), loc.getWorld());
+                    List<Location> locations = regio.getLocations();
+                    List<Location> locations2 = regio2.getLocations();
+                    locations.addAll(locations2);
+                    wallArr = locations.toArray(new Location[0]);
 
-                    case 0:
-                        dirrect = wallDirections.NORTHWEST;
-                        break;
-                    case 1:
-                        dirrect = wallDirections.SOUTHEAST;
-                        break;
-                    case 2:
-                        dirrect = wallDirections.SOUTHWEST;
-                        break;
-                    case 3:
-                        dirrect = wallDirections.NORTHEAST;
-                        break;
+                } else if (dirrect == wallDirections.SOUTHWEST) {
+                    MagePluginRegion regio = new MagePluginRegion(new Location(loc.getWorld(), loc.getBlockX(), loc.getBlockY(), loc.getBlockZ()), new Location(loc.getWorld(), loc.getBlockX() + 2, loc.getBlockY() + 2, loc.getBlockZ()), loc.getWorld());
+                    MagePluginRegion regio2 = new MagePluginRegion(new Location(loc.getWorld(), loc.getBlockX(), loc.getBlockY(), loc.getBlockZ()), new Location(loc.getWorld(), loc.getBlockX(), loc.getBlockY() + 2, loc.getBlockZ() - 2), loc.getWorld());
+                    List<Location> locations = regio.getLocations();
+                    List<Location> locations2 = regio2.getLocations();
+                    locations.addAll(locations2);
+                    wallArr = locations.toArray(new Location[0]);
+
+                } else {
+                    MagePluginRegion regio = new MagePluginRegion(new Location(loc.getWorld(), loc.getBlockX(), loc.getBlockY(), loc.getBlockZ()), new Location(loc.getWorld(), loc.getBlockX() + 2, loc.getBlockY() + 2, loc.getBlockZ()), loc.getWorld());
+                    MagePluginRegion regio2 = new MagePluginRegion(new Location(loc.getWorld(), loc.getBlockX(), loc.getBlockY(), loc.getBlockZ()), new Location(loc.getWorld(), loc.getBlockX(), loc.getBlockY() + 2, loc.getBlockZ() + 2), loc.getWorld());
+                    List<Location> locations = regio.getLocations();
+                    List<Location> locations2 = regio2.getLocations();
+                    locations.addAll(locations2);
+                    wallArr = locations.toArray(new Location[0]);
                 }
             }
-        }
-
-        Location[] wallArr;
-        //Generate Wall
-        if (!isEdge) {
-            if (dirrect == wallDirections.NORTH || dirrect == wallDirections.SOUTH) {
-                MagePluginRegion regio = new MagePluginRegion(new Location(loc.getWorld(), loc.getBlockX(), loc.getBlockY(), loc.getBlockZ() - 2), new Location(loc.getWorld(), loc.getBlockX(), loc.getBlockY()+2, loc.getBlockZ() + 2),  loc.getWorld());
-                wallArr = (Location[]) regio.getLocations().toArray();
-            } else {
-                MagePluginRegion regio = new MagePluginRegion(new Location(loc.getWorld(), loc.getBlockX()-2, loc.getBlockY(), loc.getBlockZ()), new Location(loc.getWorld(), loc.getBlockX()+2, loc.getBlockY()+2, loc.getBlockZ()),  loc.getWorld());
-                wallArr = (Location[]) regio.getLocations().toArray();
+            for (int i = 0; i < wallArr.length; i++) {
+                if (wallArr[i].getBlock().getType() == Material.AIR || wallArr[i].getBlock().getType() == Material.GRASS) {
+                    wallArr[i].getBlock().setType(Material.SNOW_BLOCK);
+                }
             }
-        } else {
-            if (dirrect == wallDirections.NORTHEAST) {
-                MagePluginRegion regio = new MagePluginRegion(new Location(loc.getWorld(), loc.getBlockX(), loc.getBlockY(), loc.getBlockZ()), new Location(loc.getWorld(), loc.getBlockX()-2, loc.getBlockY()+2, loc.getBlockZ()),  loc.getWorld());
-                MagePluginRegion regio2 = new MagePluginRegion(new Location(loc.getWorld(), loc.getBlockX(), loc.getBlockY(), loc.getBlockZ()), new Location(loc.getWorld(), loc.getBlockX(), loc.getBlockY()+2, loc.getBlockZ()+2),  loc.getWorld());
-                List<Location> locations = regio.getLocations();
-                List<Location> locations2 = regio2.getLocations();
-                locations.addAll(locations2);
-                wallArr = (Location[]) locations.toArray();
-            } else if (dirrect == wallDirections.NORTHWEST) {
-                MagePluginRegion regio = new MagePluginRegion(new Location(loc.getWorld(), loc.getBlockX(), loc.getBlockY(), loc.getBlockZ()), new Location(loc.getWorld(), loc.getBlockX()-2, loc.getBlockY()+2, loc.getBlockZ()),  loc.getWorld());
-                MagePluginRegion regio2 = new MagePluginRegion(new Location(loc.getWorld(), loc.getBlockX(), loc.getBlockY(), loc.getBlockZ()), new Location(loc.getWorld(), loc.getBlockX(), loc.getBlockY()+2, loc.getBlockZ()-2),  loc.getWorld());
-                List<Location> locations = regio.getLocations();
-                List<Location> locations2 = regio2.getLocations();
-                locations.addAll(locations2);
-                wallArr = (Location[]) locations.toArray();
-
-            } else if (dirrect == wallDirections.SOUTHWEST) {
-                MagePluginRegion regio = new MagePluginRegion(new Location(loc.getWorld(), loc.getBlockX(), loc.getBlockY(), loc.getBlockZ()), new Location(loc.getWorld(), loc.getBlockX()+2, loc.getBlockY()+2, loc.getBlockZ()),  loc.getWorld());
-                MagePluginRegion regio2 = new MagePluginRegion(new Location(loc.getWorld(), loc.getBlockX(), loc.getBlockY(), loc.getBlockZ()), new Location(loc.getWorld(), loc.getBlockX(), loc.getBlockY()+2, loc.getBlockZ()+2),  loc.getWorld());
-                List<Location> locations = regio.getLocations();
-                List<Location> locations2 = regio2.getLocations();
-                locations.addAll(locations2);
-                wallArr = (Location[]) locations.toArray();
-
-            } else {
-                MagePluginRegion regio = new MagePluginRegion(new Location(loc.getWorld(), loc.getBlockX(), loc.getBlockY(), loc.getBlockZ()), new Location(loc.getWorld(), loc.getBlockX()+2, loc.getBlockY()+2, loc.getBlockZ()),  loc.getWorld());
-                MagePluginRegion regio2 = new MagePluginRegion(new Location(loc.getWorld(), loc.getBlockX(), loc.getBlockY(), loc.getBlockZ()), new Location(loc.getWorld(), loc.getBlockX(), loc.getBlockY()+2, loc.getBlockZ()-2),  loc.getWorld());
-                List<Location> locations = regio.getLocations();
-                List<Location> locations2 = regio2.getLocations();
-                locations.addAll(locations2);
-                wallArr = (Location[]) locations.toArray();
-            }
+            return true;
         }
-        for(int i = 0; i < wallArr.length; i++) {
-            if(wallArr[i].getBlock().getType() == Material.AIR) {
-               wallArr[i].getBlock().setType(Material.SNOW_BLOCK); 
-            }
-        }
-        return true;
+        return false;
     }
 
     private enum wallDirections {
